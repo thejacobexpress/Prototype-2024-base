765ebac6491eda91fffd73b9b05aca85c7e5b519
Jacob McKee <jacobmckee06@gmail.com>
2024-11-21 14:23:34 -0800

    Update GitHub Repo with changes made offline
    Updated the github repo with the code I wrote while I did not have power.

--- composeApp/src/androidMain/kotlin/pages/WhichMatchMenu.android.kt
+++ composeApp/src/androidMain/kotlin/pages/WhichMatchMenu.android.kt
@@ -19,25 +19,193 @@ actual fun WhichMatchMenu(
     team: MutableIntState
 ) {
 
+    var hatchPanelCheck by remember { mutableStateOf(false) }
+    var lowGoalPoints by remember { mutableStateOf(0) }
+    var middleGoalPoints by remember { mutableStateOf(0) }
+    var highGoalPoints by remember { mutableStateOf(0) }
+    var totalPoints by remember { mutableStateOf(lowGoalPoints+middleGoalPoints+highGoalPoints) }
+    
     var dropDownExpanded by remember { mutableStateOf(false) }
 
     Column(
         modifier = modifier
             .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Which team is going?"
-        )
-        DropdownMenu(
-            expanded = false,
-            onDismissRequest = {
 
+        Row(
+            modifier = modifier
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            Text(
+                text = "Select a team",
+                color = getCurrentTheme().onPrimary
+            )
+
+            DropDownMenu(
+                expanded = dropDownExpanded,
+                onDismissRequest = {
+                    dropDownExpanded = !dropDownExpanded
+                }
+            ) {
+                
+                DropDownMenuItem(
+                    text = { "BlackRock" }
+                    onClick = {
+
+                    }
+                )
+
+                DropDownMenuItem(
+                    text = { "Vanguard" }
+                    onClick = {
+
+                    }
+                )
+
+                DropDownMenuItem(
+                    text = { "Fidelity" }
+                    onClick = {
+
+                    }
+                )
+
+                DropDownMenuItem(
+                    text = { "JPMorgan" }
+                    onClick = {
+
+                    }
+                )
+
+                DropDownMenuItem(
+                    text = { "State Street" }
+                    onClick = {
+
+                    }
+                )
+                
             }
-        ) { DropdownMenuItem(
+            
+        }
 
-        ) { } }
+        HorizontalDivider(
+            color = defaultPrimaryVariant,
+            thickness = 3.dp
+        )
+
+        Row(
+            modifier = modifier
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            Text(
+                text = "Did the team pull off the hatch panel?"
+                color = defaultOnPrimary
+            )
+
+            CheckBox(
+                checked = hatchPanelCheck
+                onCheckedChange = { it ->
+                    hatchPanelCheck = it
+                }
+            )
+            
+        }
+
+        Row(
+            modifier = modifier
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            Text(
+                text = "Did many points did the team score in the low goal?"
+                color = defaultOnPrimary
+            )
+
+            TextField(
+                modifier = modifier
+                    .height(55.dp),
+                value = lowGoalPoints.toString(),
+                onValueChange = { it ->
+                    try {
+                        lowGoalPoints = it.toInt()
+                    } catch (e : Exception) {
+                        Toast.makeText(context, "Please enter a number.", Toast.LENGTH_LONG).show()
+                    }
+                }
+            )
+            
+        }
+
+        Row(
+            modifier = modifier
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            Text(
+                text = "Did many points did the team score in the middle goal?"
+                color = defaultOnPrimary
+            )
+
+            TextField(
+                modifier = modifier
+                    .height(55.dp),
+                value = middleGoalPoints.toString(),
+                onValueChange = { it ->
+                    try {
+                        middleGoalPoints = it.toInt()
+                    } catch (e : Exception) {
+                        Toast.makeText(context, "Please enter a number.", Toast.LENGTH_LONG).show()
+                    }
+                }
+            )
+            
+        }
+
+        Row(
+            modifier = modifier
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            Text(
+                text = "Did many points did the team score in the high goal?"
+                color = defaultOnPrimary
+            )
+
+            TextField(
+                modifier = modifier
+                    .height(55.dp),
+                value = highGoalPoints.toString(),
+                onValueChange = { it ->
+                    try {
+                        highGoalPoints = it.toInt()
+                    } catch (e : Exception) {
+                        Toast.makeText(context, "Please enter a number.", Toast.LENGTH_LONG).show()
+                    }
+                }
+            )
+            
+        }
+
+        Row(
+            modifier = modifier
+                .fillMaxSize(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            Text(
+                text = "Total points the team scored: $totalPoints"
+                color = defaultOnPrimary
+            )
+            
+        }
+        
     }
 
 }
